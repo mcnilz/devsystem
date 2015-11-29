395078f7543776f252ef5479f7973480c1b6b100
mcnilz <mcnilz@gmail.com>
2015-11-29 14:32:50 +0100

    updated to netbeans 8.1, docker-compose 1.5.1 added php5-sqlite

--- 04-netbeans.sh
+++ 04-netbeans.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-curl -sSL http://download.netbeans.org/netbeans/8.0.2/final/bundles/netbeans-8.0.2-php-linux.sh > nb-inst.tmp.sh
-sh nb-inst.tmp.sh --silent -J-Dnb-base.installation.location=${HOME}/netbeans8
+curl -sSL http://download.netbeans.org/netbeans/8.1/final/bundles/netbeans-8.1-php-linux-x64.sh > nb-inst.tmp.sh
+sh nb-inst.tmp.sh --silent -J-Dnb-base.installation.location=${HOME}/netbeans81
 rm nb-inst.tmp.sh
